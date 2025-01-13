c9bef2e9c514af7d6baa94a46fb716e84f90f371
sandizsz <sandissirmais8@gmail.com>
2025-01-13 22:47:28 +0200

    Build Error Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, Sparkles } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 import Image from "next/image";
 import Link from "next/link";
 import { client } from "../sanity/lib/client";
